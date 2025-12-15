1fb1519aa79abd8366d253d1749aae4af5a1d47e
MohammadSameh <167872264+MohammadSameh@users.noreply.github.com>
2025-12-15 18:16:19 +0200

    Clean up comments in PostSearchSimplified.java
    Removed license comments and author tag from PostSearchSimplified.java

--- NexusGraph/src/main/java/org/example/PostSearchSimplified/src/main/java/com/mycompany/postsearchsimplified/PostSearchSimplified.java
+++ NexusGraph/src/main/java/org/example/PostSearchSimplified/src/main/java/com/mycompany/postsearchsimplified/PostSearchSimplified.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.postsearchsimplified;
 
-/**
- *
- * @author AboAmer
- */
 import java.io.*;
 import java.util.*;
 
